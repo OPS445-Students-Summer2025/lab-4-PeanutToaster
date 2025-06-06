be672816e3d2f9678c3f307fbbf1877193842a49
Ricky Tang <rtang41@myseneca.ca>
2025-06-06 14:58:15 -0400

    added comments to lab4c

--- lab4c.py
+++ lab4c.py
@@ -14,22 +14,14 @@ def create_dictionary(keys, values):
     new_dict = {}
     for index in range(len(keys)): #for each index in the range of the length of the variable keys
         new_dict[keys[index]] = values[index] #set new_dict variable to the index of the keys
-    return new_dict # return the dictionary
-    # Can also use while loop
-    '''
-    result = {}
-    index = 0
-    while index < len(keys):
-        new_dict[keys[index]] = values[index]
-        index += 1
-    return new_dict
-    '''
+    return new_dict # return the final dictionary result
 
 def shared_values(dict1, dict2):
     # Place code here - refer to function specifics in section below
     dict1_values = set(dict1.values())
     dict2_values = set(dict2.values())
     return dict1_values & dict2_values
+    #return sets containing only shared values from each dictionary
 
 if __name__ == '__main__':
     york = create_dictionary(list_keys, list_values)
